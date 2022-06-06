46a5a8b1b2c03d444b465b4cbc6f89faba8b577c
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-05 18:41:27 -0700

    created child components for footers

---
+++ src/components/Footer/Footer-Services/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FooterServices = () => {
+    return(
+        <div class="footer-column">
+            <h3>Services</h3>
+            <a href="" class="footer-link">Career Consulting</a>
+            <a href="" class="footer-link">Resume Review</a>
+            <a href="" class="footer-link">Linkedin Profile Audit</a>
+            <a href="" class="footer-link">Cover Letter Composition</a>
+            <a href="" class="footer-link">Negotiating Salaries</a>
+            <a href="" class="footer-link">Prospecting and Outreach</a>
+            <a href="" class="footer-link">Applying to Jobs</a>
+            <a href="" class="footer-link">Prepping for Interviews</a>
+        </div>
+    )
+};
+
+export default FooterServices;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FooterServices from './Footer-Services';
 
 const Footer = () => {
     return(
         <footer className='w=100 mt-auto bg-secondary p-4'>
             <div className='container'>
+                <FooterServices />
                 <h6>&copy;{new Date().getFullYear()} YKB Solutions Inc.</h6>
             </div>
         </footer>
