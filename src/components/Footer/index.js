8aa7c49663766a12e059220bdce0c497f6b47dc9
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-05 18:46:07 -0700

    imported FooterResources to Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FooterServices from './Footer-Services';
+import FooterResources from './Footer-Resources';
 
 const Footer = () => {
     return(
         <footer className='w=100 mt-auto bg-secondary p-4'>
             <div className='container'>
                 <FooterServices />
+                <FooterResources />
                 <h6>&copy;{new Date().getFullYear()} YKB Solutions Inc.</h6>
             </div>
         </footer>
