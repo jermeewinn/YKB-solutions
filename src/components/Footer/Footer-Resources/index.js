fc7a479955cafc81fc94ac731f54a4433a759e7f
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-05 18:45:12 -0700

    created resources component section for the footer

---
+++ src/components/Footer/Footer-Resources/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FooterResources = () => {
+    return(
+        <div class="footer-column">
+            <h3>Resources</h3>
+            <a href="" class="footer-link">Resume Building</a>
+            <a href="" class="footer-link">How to Create a LinkedIn Profile</a>
+            <a href="" class="footer-link">Writing a Cover Letter</a>
+            <a href="" class="footer-link">Certification Testing</a>
+            <a href="" class="footer-link">Managing your GitHub Profile</a>
+            <a href="" class="footer-link">Career Change First Steps</a>
+        </div>
+    )
+};
+
+export default FooterResources;
