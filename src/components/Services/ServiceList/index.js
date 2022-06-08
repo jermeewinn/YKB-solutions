401aebee649acaef91cc90951d353a7272950795
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-08 02:47:48 -0700

    changed descriptions for service array

--- src/components/Services/ServiceList/index.js
+++ src/components/Services/ServiceList/index.js
@@ -5,15 +5,15 @@ const ServiceList = ({  }) => {
     const [services] = useState([
         {
             name: 'Career Consulting',
-            description: 'Career consulting is our bread and butter. Here we sit you down in a one-on-one setting and get to figuring out what you want for your career in the next 5 years, and what next steps you need to take to get there.'
+            description: 'Career consulting is our bread and butter. Here we sit you down in a one-on-one setting and get to figuring out what you want for your career in the next 5 years, and what next steps you need to take to get there. Every program is different, and we tailor to your goals and needs.'
         },
         {
             name: 'Resume Review',
-            description: 'Have one of our consultants take a look at your resume, and work with you in transforming it into a key that will open doors for your future job hunt. We will be making sure that your resume will be optimized for ATS tracking, and is easy to read for human readers in your job search.'
+            description: 'Have one of our consultants take a look at your resume, and work with you in transforming it into a key that will open doors for your future job hunt. We optimize for ATS so that you can get your foot in more doors.'
         },
         {
             name: 'LinkedIn Audit',
-            description: 'The future of job hunting is online, and Linkedin is a major part of that. We will help you create and configure your LinkedIn profile so that you can start job hunting efficiently.'
+            description: 'The future of job hunting is online, and Linkedin is a major part of that. We will help you create and configure your LinkedIn profile so that you can start job hunting quickly and efficiently.'
         },
         {
             name: 'Cover Letter Composition',
