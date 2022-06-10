7bfc64e0af359f78241300c47f37e391e5e9833d
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-10 11:57:37 -0700

    created preliminary card objects for services components

--- src/components/Services/Product/index.js
+++ src/components/Services/Product/index.js
@@ -1,19 +1,28 @@
+import { requirePropFactory } from '@mui/material';
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Product = ({ service }) => {
     return(
-        <div>
-            <h4>
-                {service.name}
-            </h4>
-            <img src={require(`../../../assets/stockPhotos/${service.src}`)}
-                alt={service.name}
-                className='img-thumbnail'
-                key={service.name} />
-            <div>
-                {service.description}
-            </div>
-        </div>
+        // <div>
+        //     <h4>
+        //         {service.name}
+        //     </h4>
+        //     <img src={require(`../../../assets/stockPhotos/${service.src}`)}
+        //         alt={service.name}
+        //         className='img-thumbnail'
+        //         key={service.name} />
+        //     <div>
+        //         {service.description}
+        //     </div>
+        // </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant='top' src={require(`../../../assets/stockPhotos/${service.src}`)} />
+            <Card.Body>
+                <Card.Title>{service.name}</Card.Title>
+                <Card.Text>{service.description}</Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
