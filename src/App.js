ea506a90fa092d393b19c90db4c55defc346484d
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-11 19:39:39 -0700

    Created initial file structure for Resources component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Services from './components/Services';
+import Resources from './components/Resources';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -20,6 +21,10 @@ function App() {
     {
       name: "Services",
       description: "Here's a summary of our services"
+    },
+    {
+      name: "Resources",
+      description: "Our catalog of our resources to help you get started with your career transition"
     },
     {
       name: "Testimonials",
@@ -39,6 +44,9 @@ function App() {
     }
     if (currentCategory.name === 'Services') {
       return <Services currentCategory={currentCategory} />;
+    }
+    if (currentCategory.name === 'Resrouces') {
+      return <Resources currentCategory={currentCategory} />;
     }
     if (currentCategory.name === 'Testimonials') {
       return <Testimonials currentCategory={currentCategory} />;
