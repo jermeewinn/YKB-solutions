5516d8e3a9e46d3f483b09a7c73385c12f3f6194
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-15 22:06:57 -0700

    enabled data to populate within modal

--- src/components/Services/Product/index.js
+++ src/components/Services/Product/index.js
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
 import { Card, Col, Modal, Button } from 'react-bootstrap';
 
-function ServiceModal(props) {
-    return(
-        <Modal
-            {...props}
-            size='lg'
-            aria-labelledby='contained-modal-title-vcenter'
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id='contained-modal-title-vcenter'>
-                    {/* {service.src} */}
-                    Test
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {/* <h4>
-                    {service.name}
-                </h4>
-                <p>
-                    {service.description}
-                </p> */}
-                Test
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
-};
+
 
 const Product = ({ service }) => {
     
 
     const [modalShow, setModalShow] = React.useState(false);
+
+    function ServiceModal(props) {
+    
+        return(
+            <Modal
+                {...props}
+                size='lg'
+                aria-labelledby='contained-modal-title-vcenter'
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id='contained-modal-title-vcenter'>
+                        {service.src}
+                        Test
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>
+                        {service.name}
+                    </h4>
+                    <p>
+                        {service.description}
+                    </p>
+                    Test
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    };
 
     return(
         <Col>
