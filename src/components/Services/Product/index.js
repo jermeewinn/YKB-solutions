a94fd1c9bb72ea08e5f3de32047f90fb38d47c38
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-15 19:09:01 -0700

    changed cards to button objects w/i services, enabled to open modal

--- src/components/Services/Product/index.js
+++ src/components/Services/Product/index.js
@@ -1,18 +1,50 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Col, Modal, Button } from 'react-bootstrap';
+
+function ServiceModal(service) {
+    return(
+        <Modal
+            {...service}
+            size='lg'
+            aria-labelledby='contained-modal-title-vcenter'
+            centered
+        >
+            <Modal.Header closeButtone>
+                <Modal.Title id='contained-modal-title-vcenter'>
+                    {service.name}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>
+                    {service.description}
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onclick={service.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+};
 
 const Product = ({ service }) => {
+    
+
+    const [modalShow, setModalShow] = React.useState(false);
+
     return(
         <Col>
-            <Card style={{ width: '18rem' }} className='service-card'>
+        
+            <Button variant='secondary' onClick={() => setModalShow(true)} style={{ width: '18rem' }} className='service-card' >
                 <Card.Img variant='top' src={require(`../../../assets/stockPhotos/${service.src}`)} />
                 <Card.Body>
                     <Card.Title>{service.name}</Card.Title>
-                    <Card.Text>{service.description}</Card.Text>
+                    {/* <Card.Text>{service.description}</Card.Text> */}
                 </Card.Body>
-            </Card>
+                
+            </Button>
+            <ServiceModal show={modalShow} onHide={() => setModalShow(false)}/>
         </Col>
     )
-}
+};
 
 export default Product;
