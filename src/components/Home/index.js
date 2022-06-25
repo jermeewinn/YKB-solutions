b6d5fc45e23d0904765e25a5a435504f8f9d6f0e
Jeremy Huynh <jeahuynh18@gmail.com>
2022-06-25 02:00:02 -0700

    created preliminary layout for Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+
 export default class Home extends Component {
     render() {
         return(
             <section id='home'>
-                <div>
-                    <h1>Home</h1>
-                    <p>Sup</p>
+                <div id='home-banner'>
+                    <h1>Don't Live to Work. Work to Live.</h1>
+                    <h3>Whether you're graduating from college, or have been in the workforce for awhile, we'll help with the transition </h3>
+                </div>
+                <div id='logo-array'>
+                    <a href='/testimonials'>
+                        <img src='src\assets\logos\uc_davis.png' alt='UC Davis Logo' />
+                    </a>
+                    <a href='/testimonials'>
+                        <img src='src\assets\logos\moss_adams/pmg' alt='Moss Adams Logo' />
+                    </a>
                 </div>
             </section>
         )
