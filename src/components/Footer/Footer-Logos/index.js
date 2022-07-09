a8a325b17877c914b63b58648c012956478c09ee
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-08 17:36:18 -0700

    enabled logo-hr to render

--- src/components/Footer/Footer-Logos/index.js
+++ src/components/Footer/Footer-Logos/index.js
@@ -7,7 +7,7 @@ import { BsTwitter } from 'react-icons/bs';
 const FooterLogos = () => {
     return(
         <div className="footer-logo-row flex-fill">
-            <div className="hr flex-fill">
+            <div className="logo-hr flex-fill">
                 <hr/>
             </div>
             <h3 className="footer-logo-content">
@@ -17,11 +17,15 @@ const FooterLogos = () => {
                 <a href="" className="footer-logo"><BsTwitter /></a>&nbsp;
                 <a href="" className="footer-logo"><ImFacebook /></a>&nbsp;
             </h3>
-            <div className="hr flex-fill">
+            <div className="logo-hr flex-fill">
                 <hr/>
             </div>
         </div>
     );
 };
+
+//NOTE TO SELF: Lines before and after footer-logo-content are created by <hr/> tags. 
+//Normally they would act as subject breaks that would span the entire length of the screen, but if you were to treat them w/ flex-fill, display: flex, and justify-content: space-between,
+//they would be treated as three children to a parent div.
 
 export default FooterLogos;
