f242a0895c3dd532ecff1ec14d965c3cb2da5492
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-12 14:48:00 -0700

    created Banner child component w/i Home component

---
+++ src/components/Home/Banner/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Banner() {
+    return(
+        <container>
+            <div id='home-banner'>
+                <h1>Don't Live to Work. Work to Live.</h1>
+                <h3>Whether you're graduating from college, or have been in the workforce for awhile, we'll help with the transition </h3>
+            </div>
+        </container>
+    )
+}
+
+export default Banner;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Banner from '../Home/Banner';
 import LogoArray from '../Home/Logo-Array';
 
 export default class Home extends Component {
     render() {
         return(
             <section id='home'>
-                <div id='home-banner'>
-                    <h1>Don't Live to Work. Work to Live.</h1>
-                    <h3>Whether you're graduating from college, or have been in the workforce for awhile, we'll help with the transition </h3>
-                </div>
+                <Banner />
                 <LogoArray />
             </section>
         )
