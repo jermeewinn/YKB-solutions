20331251d52773136808e68a83b1c6e29edad09c
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-12 15:45:25 -0700

    simplified margin CSS properties for .home-logo

--- src/components/Home/Logo-Array/index.js
+++ src/components/Home/Logo-Array/index.js
@@ -3,12 +3,13 @@ import uc_davis from '../../../assets/logos/uc_davis.png';
 import moss_adams from '../../../assets/logos/moss_adams.png';
 import bluevine from '../../../assets/logos/bluevine.png';
 import gridsme from '../../../assets/logos/gridsme.png';
+import pan from '../../../assets/logos/palo_alto_networks.png';
 
 function LogoArray() {
     return(
-        <container>
-            <h5>Some companies our clients work at now</h5>
-            <div id='logo-array'>
+        <container id='logo-array' className='home-logo-array'>
+            <h2>Some companies our clients work at now</h2>
+            <div>
                 <a href='/testimonials'>
                     <img src={uc_davis} alt='UC Davis Logo' className='home-logo' />
                 </a>
@@ -20,6 +21,9 @@ function LogoArray() {
                 </a>
                 <a href='/testimonials'>
                     <img src={gridsme} alt='GridSME Capital Logo' className='home-logo' />
+                </a>
+                <a href='/testimonials'>
+                    <img src={pan} alt='GridSME Capital Logo' className='home-logo' />
                 </a>
             </div>
         </container>
