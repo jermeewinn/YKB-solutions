a0aa0a8e708440d9758c2bd995281991055bc8c6
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-12 16:54:41 -0700

    switched from background transition to opacity transition upon hover over logos in logo-array

--- src/components/Home/Logo-Array/index.js
+++ src/components/Home/Logo-Array/index.js
@@ -9,36 +9,37 @@ import alation from '../../../assets/logos/alation.png';
 import skedulo from '../../../assets/logos/skedulo.webp';
 import tai_software from '../../../assets/logos/tai_software.png';
 
+//NOTE TO SELF: See if there's a more efficient way of creating the logo-array while keeping the styling we have.
 function LogoArray() {
     return(
         <container id='logo-array' className='home-logo-array'>
             <h2>Some companies our clients work at now</h2>
             <div>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={uc_davis} alt='UC Davis Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={moss_adams} alt='Moss Adams Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={bluevine} alt='BlueVine Capital Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={gridsme} alt='GridSME Capital Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={pan} alt='Palo Alto Networks Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={armis} alt='Armis Security Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={alation} alt='Alation Inc. Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={skedulo} alt='Skedulo Logo' className='home-logo' />
                 </a>
-                <a href='/testimonials'>
+                <a href='/testimonials' className='home-logo-background'>
                     <img src={tai_software} alt='Tai Software Logo' className='home-logo' />
                 </a>
             </div>
