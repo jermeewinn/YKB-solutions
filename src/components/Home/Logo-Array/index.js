9abd4101098b46ecf2a4411dbb5d9904a5e13745
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-12 15:05:00 -0700

    rendered uc_davis & moss_adams logos for logo array

--- src/components/Home/Logo-Array/index.js
+++ src/components/Home/Logo-Array/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import uc_davis from '../../../assets/logos/uc_davis.png';
+import moss_adams from '../../../assets/logos/moss_adams.png';
 
 function LogoArray() {
     return(
@@ -6,10 +8,10 @@ function LogoArray() {
             <h5>Here are some of the companies our clients work at</h5>
             <div id='logo-array'>
                 <a href='/testimonials'>
-                    <img src='src\assets\logos\uc_davis.png' alt='UC Davis Logo' />
+                    <img src={uc_davis} alt='UC Davis Logo' className='home-logo' />
                 </a>
                 <a href='/testimonials'>
-                    <img src='src\assets\logos\moss_adams/pmg' alt='Moss Adams Logo' />
+                    <img src={moss_adams} alt='Moss Adams Logo' className='home-logo' />
                 </a>
             </div>
         </container>
