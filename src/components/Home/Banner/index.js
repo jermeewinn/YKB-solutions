887ab6251abd31b917d65c93f3c8350128849c79
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-13 20:17:46 -0700

    styled home-banner-text

--- src/components/Home/Banner/index.js
+++ src/components/Home/Banner/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 function Banner() {
     return(
         <container className='home-banner'>
-            <div id='home-banner'>
-                <h1 className='my-5'>Don't Live to Work. Work to Live.</h1>
-                <h3>Whether you're graduating from college, or have been in the workforce for awhile, we'll help with the transition </h3>
+            <div className='home-banner-left'>
+                <div className='home-banner-text'>
+                    <h1 className='my-5'>Don't Live to Work. Work to Live.</h1>
+                    <h5 className='my-5'>Whether you're graduating from college, or have been in the workforce for awhile, we'll help with the transition.</h5>
+                </div>
             </div>
             <div></div>
-            <div className='home-banner-curve'></div>
+            <div className='home-banner-right'></div>
         </container>
     )
 }
