65f5274dd3ef7fd3c3bf62c2d780917d66c82e5a
Jeremy Huynh <jeahuynh18@gmail.com>
2022-08-23 22:28:06 -0700

    changes to client information

--- src/components/Testimonials/TestimonialList/index.js
+++ src/components/Testimonials/TestimonialList/index.js
@@ -34,8 +34,8 @@ const TestimonialList = ({ tech }) => {
       image: "Jeremy-Huynh-pfp.jpg",
       height: "350px",
       width: "300px",
-      title: "Freelance Full-Stack Software Developer",
-      company: "Self-employed",
+      title: "Permit Technician/Freelance Full-Stack Software Developer",
+      company: "Bureau Veritas North America",
       link: "https://www.linkedin.com/in/jeremy-huynh/",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
